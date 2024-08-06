fc318490e0e4f9d3fdf24bf640a75ca157bf198f
rachitupadhyay1 <rachit.upadhyay@atos.net>
2024-08-06 15:26:01 +0000

    Add aws_instance test1 resource in main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
 provider "aws" {
-  region = "us-east-1"  # Adjust the region as needed
+  region = "us-east-1"
 }
- 
-resource "aws_instance" "example" {
-  ami           = "ami-0b72821e2f351e396"
+
+resource "aws_instance" "test1example" {
+  ami = "ami-0b72821e2f351e396"
   instance_type = "t2.micro"
+  key_name = "AKIAQIJRSMDKDCT7ARCZ" 
+
+#  network_interface {
+#    device_index = 0
+#    associate_public_ip_address = true 
+#    subnet_id = "subnet-XXXXXXXX" 
+#  }
+
+  tags = {
+    Name = "MyInstanceTest"
+  }
 }
